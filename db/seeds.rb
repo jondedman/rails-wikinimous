d0e9429d6cbaa5f2eaee86301b3102a74711bd43
Jon Dedman <jonathandedman@googlemail.com>
2023-01-23 13:14:10 +0000

    implement all crud - todo - front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Hipster.unique.sentence(word_count: 3, supplemental: true),
+    content: Faker::Hipster.unique.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4)
+  )
+end
+#  t.string "title"
+#     t.text "content"
+
+#     Faker::Name.unique.name # This will return a unique name every time it is called
